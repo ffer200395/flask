6d2c0c7b00fc80828e1ecb6552f60487d1b7b5ac
ffer <ffer200395@gmail.com>
2023-11-28 15:30:04 +0100

    initial commit

---
+++ src/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import os
+
+import pickle
+
+app = Flask(__name__)
+
+# Cargar el modelo preentrenado
+model = pickle.load(open('models/iris_model.pkl', "rb"))
+
+# Página de inicio
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Predicción
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            if 'file' in request.files:
+                file = request.files['file']
+                data = pd.read_csv(file)
+            else:
+                sepal_length = float(request.form['sepal_length'])
+                sepal_width = float(request.form['sepal_width'])
+                petal_length = float(request.form['petal_length'])
+                petal_width = float(request.form['petal_width'])
+                data = pd.DataFrame({
+                    'sepal_length': [sepal_length],
+                    'sepal_width': [sepal_width],
+                    'petal_length': [petal_length],
+                    'petal_width': [petal_width]
+                })
+                
+            prediction = model.predict(data)
+            return f'La predicción es: {prediction}'
+        except Exception as e:
+            return f'Error: {e}'
+    return 'Error en la predicción'
+
+if __name__ == '__main__':
+    app.run(debug=True)
